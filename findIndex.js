0d94022fb0dfa8eb273698874c998ed8dc630887
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-14 16:13:33 +0500

    Create findIndex.js

---
+++ findIndex.js
@@ -0,0 +1,10 @@
+var strStr = function(haystack, needle) {
+    if(haystack.includes(needle)){
+      return haystack.indexOf(needle)
+    }else{
+      return -1
+    }
+};
+
+
+console.log(strStr('sadbutsad','aa'));
