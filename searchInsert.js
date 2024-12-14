cfe2b69a82dfd705d825b9d0e6afb6ad383d6f27
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-14 16:16:50 +0500

    Update searchInsert.js

--- searchInsert.js
+++ searchInsert.js
@@ -1,22 +1,11 @@
 var searchInsert = function(nums, target) {
-  let s = 0;
-  for(let i = 0; i<=nums.length-1;i++){
-    if(nums[i]===target){
-      s++
-      return i
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;  
+    }
+    if (nums[i] > target) {
+      return i;  
     }
   }
-  if(s===0){
-    for(let i = 0; i<=nums.length-1;i++){
-      if(nums[i]>target){
-        s++
-        return i
-      }
-    }
-  }
-  if(s===0){
-    return nums.length
-  }
+  return nums.length;  
 };
-console.log(searchInsert([1,3,5,6],7));
-// searchInsert([1,3,5,6],5)
